8e9cb5b4a79675567a0f4a193b3842e6282b00b5
kuei <momonbin1991@icloud.com>
2025-02-11 15:00:16 +0800

    20250211 更新

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="my-20 container px-10 lg:mx-auto lg:max-x-[90rem] flex flex-col lg:gap-16 gap-[4.75rem]"
+		class="my-20 container px-10 lg:mx-auto lg:max-x-[90rem] flex flex-col lg:gap-16 gap-[4.75rem] h-screen"
 	>
 		<explain-label></explain-label>
 		<card-group></card-group>
